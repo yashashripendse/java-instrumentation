885048d929fb780a9b1f7f496493d964514ffa13
Yashashiri Pendse <yashashiripendse@Yashashris-MacBook-Pro.local>
2020-07-31 15:20:30 -0400

    fixed agent.jar to include class files

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     `java-library`
 }
+
+tasks.register<Jar>("agentJarTask"){
+    manifest{
+        attributes(mapOf("Premain-Class" to "com.github.yashashripendse.Agent", "Can-Redefine-Classes" to "true"))
+    }
+    archiveClassifier.set("with-manifest")
+
+    from(sourceSets.main.get().output)
+}
