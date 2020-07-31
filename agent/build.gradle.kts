932c071bb39d60189b6bfa410a26609fb06bc300
Yashashiri Pendse <yashashiripendse@Yashashris-MacBook-Pro.local>
2020-07-31 15:52:21 -0400

    added asm as a dependency to the agent

--- agent/build.gradle.kts
+++ agent/build.gradle.kts
@@ -8,5 +8,15 @@ tasks.register<Jar>("agentJarTask"){
     }
     archiveClassifier.set("with-manifest")
 
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+
     from(sourceSets.main.get().output)
+}
+
+dependencies{
+    implementation("org.ow2.asm", "asm", "8.0.1")
+    implementation("org.ow2.asm", "asm-commons", "8.0.1")
 }
